7a56172a8d4a1122aca745ee66d1848078f53cb7
Max Zhou <maxonezhou@gmail.com>
2020-04-24 11:23:44 -0600

    Init

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,231 @@
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null
+};
+
+var spotlight = {
+    distance: '0px',
+    opacity: 0,
+    delay: 100
+};
+
+function myCallback(scroll, name) {
+    scroll.clean(name);
+}
+
+// Navbar effects
+
+scroll.reveal('.navbar-brand', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                cleanup: true
+                                });
+
+scroll.reveal('.nav1', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 300,
+                                cleanup: true
+                                });
+scroll.reveal('.nav2', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 400,
+                                cleanup: true
+                                });
+scroll.reveal('.nav3', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 500,
+                                cleanup: true
+                                });
+scroll.reveal('.nav4', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 600,
+                                cleanup: true
+                                });
+scroll.reveal('.nav5', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 700,
+                                cleanup: true
+                                });
+scroll.reveal('.nav6', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 800,
+                                cleanup: true
+                                });
+
+
+// Intro effects
+
+scroll.reveal('.intro1', {distance: '50%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 900,
+                                cleanup: true
+                                });
+scroll.reveal('.intro2', {distance: '50%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1000,
+                                cleanup: true
+                                });
+scroll.reveal('.intro3', {distance: '50%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1100,
+                                cleanup: true
+                                });
+scroll.reveal('.intro4', {distance: '50%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1200,
+                                cleanup: true
+                                });
+scroll.reveal('.intro-btn', {distance: '50%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1300,
+                                cleanup: true
+                                });
+scroll.reveal('.intro-img', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 900,
+                                cleanup: true
+                                });
+scroll.reveal('.transition-div', {distance: '50%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1400,
+                                cleanup: true
+                                });
+
+// Intro Icona
+scroll.reveal('.github', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1400,
+                                cleanup: true
+                                });
+scroll.reveal('.linkedin', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1500,
+                                cleanup: true
+                                });
+scroll.reveal('.devpost', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1600,
+                                cleanup: true
+                                });
+scroll.reveal('.email', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1700,
+                                cleanup: true
+                                });
+scroll.reveal('.vertical-line2', {distance: '150%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1800,
+                                cleanup: true
+                                });
+
+// About me on landing page
+
+scroll.reveal('.about-me-img', {distance: '20%',
+                                        origin: 'bottom',
+                                        opacity: 0,
+                                        delay: 300,
+                                        cleanup: true
+                                        });
+scroll.reveal('.about-me1', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 400,
+                                cleanup: true
+                                });
+scroll.reveal('.about-me2', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 500,
+                                cleanup: true
+                                });
+scroll.reveal('.quote', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 600,
+                                cleanup: true
+                                });
+scroll.reveal('.about-me3', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 700,
+                                cleanup: true
+                                });
+scroll.reveal('.about-me4', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 800,
+                                cleanup: true
+                                });
+scroll.reveal('.about-me5', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 900,
+                                cleanup: true
+                                });
+scroll.reveal('.about-me-btn', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 1000,
+                                cleanup: true
+                                });
+
+// Experience Section
+
+scroll.reveal('.experience-title', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 300,
+                                cleanup: true
+                                });
+scroll.reveal('.ibm', {distance: '20%',
+                                        origin: 'bottom',
+                                        opacity: 0,
+                                        delay: 300,
+                                        cleanup: true
+                                        });
+scroll.reveal('.cansofcom', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 300,
+                                cleanup: true
+                                });
+scroll.reveal('.ford', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 300,
+                                cleanup: true
+                                });
+scroll.reveal('.wato', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 300,
+                                cleanup: true
+                                });
+scroll.reveal('.uofc', {distance: '20%',
+                                origin: 'bottom',
+                                opacity: 0,
+                                delay: 300,
+                                cleanup: true
+                                });
+
+//scroll.destroy();
+//scroll.reveal('.about-me-img', spotlight);*/
